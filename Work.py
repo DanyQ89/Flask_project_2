552ec055ae3aa686b2f5a7327a1cbe1af6a4ec5e
DanyQ89 <danila.shornikov@icloud.com>
2024-03-01 19:12:27 +0300

    Meow_2
    last version

--- Work.py
+++ Work.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, redirect
+import json
+from random import randint
+
+from flask import Flask, render_template, redirect, request
 
 from Forms import LoginForm
 
-app = Flask(__name__, static_folder='./static')
+app = Flask(__name__)
 
+
+# d = {'1': {'n_s': 'Энди Уир', 'prof': 'Астрогеолог'}, '2': {'n_s': 'Леша', 'prof': 'пчела'}}
+# with open('./templates/data.json', 'wt', encoding='utf8') as file:
+#     json.dump(d, file, ensure_ascii=False, indent=4)
 
 @app.route('/index/<title>')
 def index(title):
@@ -46,7 +53,7 @@ def login():
         try:
             f = form.data
             print(f)
-        except Exception():
+        except Exception:
             pass
         return redirect('/success')
 
@@ -57,15 +64,55 @@ def login():
 def success():
     return 'Поздравляем с отправкой формы'
 
+
 @app.route('/distribution')
 def distribution():
     arr = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth']
     return render_template('distr.html', arr=arr)
 
+
 @app.route('/table_param/<sex>/<int:age>')
 def table_param(sex, age):
     print(sex, age)
-    return render_template('kauta.html')
+    color = 'white'
+    photo = '../../static/img/'
+
+    if age < 21:
+        if sex == 'male':
+            color = 'lightgrey'
+        elif sex == 'female':
+            color = '#FFDAB9'
+    else:
+        if sex == 'male':
+            color = 'blue'
+        elif sex == 'female':
+            color = 'red'
+    if sex == 'male':
+        photo += 'male.png'
+    elif sex == 'female':
+        photo += 'female.png'
+    return render_template('kauta.html', color=color, photo=photo)
+
+
+@app.route('/member')
+def member():
+    with open('./templates/data.json', encoding='utf8') as file:
+        a = json.load(file)
+    rand_member = dict(sorted(list(a[str(randint(1, len(a)))].items())))
+
+    return render_template('member.html', **rand_member)
+
+
+arr = ['male.png', 'female.png']
+
+
+@app.route('/gallery', methods=['GET', 'POST'])
+def gallery():
+    if request.method == 'POST':
+        file = request.files['filename']
+        arr.append(file.filename)
+    return render_template('carousel.html', arr=arr)
+
 
 if __name__ == '__main__':
     app.run(port=8888, host='127.0.0.1', debug=True)
